4de2e6f84e053e778c917ba830f0aa761b610e2e
Hayder7Aly <007harryjutt@gmail.com>
2022-03-27 14:14:03 +0500

    add functionality to enable hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  images: {
+    default: "loader",
+    domains: ["strapi-mind-blogs.herokuapp.com"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
